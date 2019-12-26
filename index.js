985f7e856201f1e02e40f5c7a81179ff1651772a
Henrique Cesar <hcesarjs@gmail.com>
2019-12-25 23:36:58 -0300

    feat: add google images search

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const bots = {
     input: require('./bots/input'),
     text: require('./bots/text'),
-    state: require('./bots/state')
+    state: require('./bots/state'),
+    image: require('./bots/image')
 }
 
 async function start() {
-    bots.input()
-    await bots.text();
+    //bots.input()
+    //await bots.text();
+    await bots.image();
 
     const content = bots.state.load();
     console.dir(content, { depth: null });
